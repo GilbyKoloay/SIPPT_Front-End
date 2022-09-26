9325d3859d0d9ec214699ae0ff08ba3bff9085b6
GilbyKoloay <lolwtf10001@gmail.com>
2022-09-26 20:39:43 +0800

    start the project again (not from scratch)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-// pages
-import {
-  Login,
-  Loket,
-  NotFound,
-} from './pages';
-
 export default function App() {
-  const [__token, __setToken] = useState(null);
-
   return (
-    <div className="App">
+    <div className='App'>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login props={ { __token, __setToken } } />} />
-          <Route path='/loket' element={__token ? <Loket props={ { __token, __setToken } } /> : <NotFound />} />
-          <Route path='*' element={<NotFound />} />
+          
         </Routes>
       </BrowserRouter>
     </div>
