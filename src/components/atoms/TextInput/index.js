4209a73c0f281f20e75f31b9dc0af571f2a6954c
GilbyKoloay <lolwtf10001@gmail.com>
2022-09-27 11:07:05 +0800

    modified 'TextInput' atoms for disabled option

--- src/components/atoms/TextInput/index.js
+++ src/components/atoms/TextInput/index.js
@@ -2,13 +2,13 @@
 import './styles.css';
 
 export default function TextInput({ props }) {
-  const { label, errMsg } = props;
+  const { label, errMsg, disabled } = props;
 
   return(
     <div className='TextInput'>
       <div className='textLabel'>{label}</div>
       <div className='input'>
-        <input type='text' className='textInputt textInput' />
+        <input type='text' className={`textInputt${disabled ? '-disabled' : ''} textInput`} disabled={disabled} />
         <div className='textErrMsg'>{errMsg}lorem ipsum dolor sit amet consectetur adispicing elit</div>
       </div>
     </div>
