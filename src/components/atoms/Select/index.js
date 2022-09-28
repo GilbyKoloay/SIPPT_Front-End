cc33e971a99831fff513bb13742916a3da4e2e15
GilbyKoloay <lolwtf10001@gmail.com>
2022-09-28 15:26:50 +0800

    slight improvement in 'Select' atom

--- src/components/atoms/Select/index.js
+++ src/components/atoms/Select/index.js
@@ -8,7 +8,12 @@ export default function Select({ props }) {
     <div className='atom-select'>
       <div className='textLabel'>{label}</div>
       <div className='input'>
-        <select className='textInput' />
+        {/* <select className={`textInput ${'err'}`}> */}
+        <select className={`textInput`}>
+          <option value='test'>test1</option>
+          <option value='test'>test2</option>
+          <option value='test'>test3</option>
+        </select>
         <div className='textErrMsg'>{errMsg}error messages</div>
       </div>
     </div>
