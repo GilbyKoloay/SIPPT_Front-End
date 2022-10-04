02cc3a06cf6dab48870284aeeb6980c6bfd5fba9
GilbyKoloay <lolwtf10001@gmail.com>
2022-10-05 00:46:36 +0800

    integrated values to 'Select' atom

--- src/components/atoms/Select/index.js
+++ src/components/atoms/Select/index.js
@@ -2,14 +2,14 @@
 import './styles.css';
 
 export default function Select({ props }) {
-  const { label, errMsg, tab, options } = props;
+  const { label, value, options, onChange, errMsg, tab } = props;
 
   return(
     <div className='atom-select'>
       <div className={`textLabel ${tab && 'tab'}`}>{label}</div>
       <div className='input'>
         {/* <select className={`textInput ${'err'}`}> */}
-        <select className={`textInput`} value={options[0]}>
+        <select className={`textInput`} value={(value === '') ? options[0] : value} onChange={e => onChange(e.target.value)}>
           {options.map((o, index) => (
             <option key={index} value={o} disabled={(index === 0)}>{o}</option>
           ))}
