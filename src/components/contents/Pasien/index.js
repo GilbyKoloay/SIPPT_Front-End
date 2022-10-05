7b2d98a4fc998adfe42db976d4b17c8607818573
GilbyKoloay <lolwtf10001@gmail.com>
2022-10-06 02:02:54 +0800

    add FindPatient content in Patient page

--- src/components/contents/Pasien/index.js
+++ src/components/contents/Pasien/index.js
@@ -8,9 +8,16 @@ import {
   Gap,
   HorLine,
   TextInput,
+  RadioButton,
+  Select,
+  TextAreaInput,
 } from '../../atoms';
 
-export default function Pasien() {
+export default function Pasien({ props }) {
+  const {
+    addressList, sexList, religionList, maritalStatusList, jobList, paymentMethodList, JKNList,
+  } = props;
+
   return(
     <main>
       <div className='contentSplit'>
@@ -23,8 +30,123 @@ export default function Pasien() {
           <Button props={{title: 'Cari Menggunakan BPJS/KIS'}} />
           <Gap props={{height: '25px'}} />
           <HorLine />
+          <Gap props={{height: '25px'}} />
+          { // Cari Menggunakan Nomor Rekam Medis
+            <form className='col'>
+              <TextInput props={{
+                label: 'No. Rekam Medis', 
+              }} />
+            </form>
+          }
+          { // Cari Menggunakan Data Diri
+            <form className='col'>
+              <TextInput props={{
+                label: 'Nama', 
+              }} />
+              <RadioButton props={{
+                label: 'Jenis Kelamin', 
+                options: sexList, 
+              }} />
+              <div className='textLabel' style={{marginBottom: 15}}>Alamat: </div>
+              <Select props={{
+                label: 'Kab. / Kota', 
+                options: ['(KABUPATEN / KOTA)'], 
+                tab: true,
+              }} />
+              <Select props={{
+                label: 'Kec.', 
+                options: ['(KECAMATAN)'], 
+                tab: true, 
+              }} />
+              <Select props={{
+                label: 'Kel. / Desa', 
+                options: ['(KELURAHAN / DESA)'], 
+                tab: true,
+              }} />
+              <TextInput props={{
+                label: 'No. Telepon', 
+              }} />
+              <TextInput props={{
+                label: 'Tempat Lahir', 
+              }} />
+              <TextInput props={{
+                label: 'Tanggal Lahir', 
+              }} />
+              <TextInput props={{
+                label: 'Tanggal Lahir', 
+              }} />
+              <TextInput props={{
+                label: 'Tanggal Lahir', 
+              }} />
+              <TextInput props={{
+                label: 'Umur', 
+              }} />
+              <TextInput props={{
+                label: 'Umur', 
+              }} />
+              <TextInput props={{
+                label: 'Umur', 
+              }} />
+              <TextInput props={{
+                label: 'Nama KK', 
+              }} />
+              <Select props={{
+                label: 'Agama', 
+                options: ['(AGAMA)', ...religionList], 
+              }} />
+              <Select props={{
+                label: 'Status', 
+                options: ['(STATUS)', ...maritalStatusList], 
+              }} />
+              <Select props={{
+                label: 'Pekerjaan', 
+                options: ['(PEKERJAAN)', ...jobList], 
+              }} />
+              <RadioButton props={{
+                label: 'Cara Pembayaran', 
+                options: paymentMethodList, 
+              }} />
+              <RadioButton props={{
+                label: 'JKN', 
+                options: JKNList, 
+              }} />
+              <TextInput props={{
+                label: 'Umur', 
+              }} />
+            </form>
+          }
+          { // Cari Menggunakan BPJS/KIS
+            <form className='col'>
+              <TextInput props={{
+                label: '', 
+              }} />
+              <TextInput props={{
+                label: '', 
+              }} />
+              <TextInput props={{
+                label: 'Tanggal Lahir', 
+              }} />
+              <TextInput props={{
+                label: 'Tanggal Lahir', 
+              }} />
+              <TextInput props={{
+                label: 'Faskes Tingkat I', 
+              }} />
+              <TextInput props={{
+                label: 'Kelas Rawat', 
+              }} />
+              <TextInput props={{
+                label: 'NIK', 
+              }} />
+              <TextAreaInput props={{
+                label: 'Alamat', 
+              }} />
+            </form>
+          }
         </div>
-        <div className='content'>right</div>
+        <div className='content'>
+          right
+        </div>
       </div>
     </main>
   );
