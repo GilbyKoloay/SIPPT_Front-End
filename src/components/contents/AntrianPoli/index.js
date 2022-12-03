a2ed6075be4b9ba752fae121cea671e02a6c4d54
tarishahamzah <christian.sahelangi@lumoshive.com>
2022-12-04 04:35:48 +0700

    Update Antrian Poli Frontend

--- src/components/contents/AntrianPoli/index.js
+++ src/components/contents/AntrianPoli/index.js
@@ -1,31 +1,112 @@
-import React from 'react';
-import { Button } from '../../atoms';
+import React from "react";
+import ReactToPrint from "react-to-print";
+// import { Title } from "../../atoms";
+// import PropTypes from "prop-types";
+import './style.css';
 
-export default function AntrianPoli() {
-  
-  
-  return(
-    <main>
-      <div className='content'>
+
+class AntrianPoli extends React.Component {
+
+  render() {
+     
+    return (
+      <main>
+      <div className="content">
+      <ReactToPrint
+          trigger={() => <button href="#">Cetak Antrian Poli UMUM</button>}
+          content={() => this.componentRef}
+        />
+        <PoliUmum ref={el => (this.componentRef = el)} />
+      <ReactToPrint
+        trigger={() => <button href="#">Cetak Antrian Poli Gigi</button>}
+        content={() => this.componentRef}
+      />
+      <PoliGigi ref={el => (this.componentRef = el)} />
+      <ReactToPrint
+        trigger={() => <button href="#">Cetak Antrian Poli KIA</button>}
+        content={() => this.componentRef}
+      />
+      <PoliKia ref={el => (this.componentRef = el)} />
+    </div>
+      </main>
       
-       <Button props={{
-          label:'Cetak Antrian Poli Umum',
-          type:'primary',
-        }} />
-      
-      <br />
-      <Button props={{
-          label:'Cetak Antrian Poli Anak',
-          type:'primary',
-        }} />
-        
-      <br />
-      <Button props={{
-          label:'Cetak Antrian Poli Gigi',
-          type:'primary',
-        }} />
-      
+    );
+  }
+}
+
+export default AntrianPoli;
+
+class PoliUmum extends React.Component {
+  render() {
+    return (
+      <react-fragment>
+        <div id="content" className="row">
+        <div className="content">
+        <table id="antrian">
+        <thead>
+          <th>Antrian Poli Umum No</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>A.01</td>
+          </tr>
+        </tbody>
+      </table>
       </div>
-    </main>
-  );
+        </div>
+      </react-fragment>
+      
+      
+    );
+  }
+}
+
+class PoliGigi extends React.Component {
+  render() {
+    return (
+
+      <react-fragment>
+        <div id="content" className="row">
+        <div className="content">
+        <table id="antrian">
+        <thead>
+          <th>Antrian Poli Gigi No</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>B.01</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+        </div>
+      </react-fragment>
+      
+    );
+  }
+}
+
+class PoliKia extends React.Component {
+  render() {
+    return (
+
+      <react-fragment>
+        <div id="content" className="row">
+        <div className="content">
+        <table id="antrian">
+        <thead>
+          <th>Antrian Poli KIA No</th>
+        </thead>
+        <tbody>
+          <tr>
+            <td>C.01</td>
+          </tr>
+        </tbody>
+      </table>
+      </div>
+        </div>
+      </react-fragment>
+      
+    );
+  }
 }
