4ffa7c9b9b1caddceb30c36889911d6c7839841e
Tarisha Hamzah <s21810347@student.unklab.ac.id>
2022-12-03 20:17:05 +0800

    UPdate count antrian poli

--- src/components/contents/AntrianPoli/index.js
+++ src/components/contents/AntrianPoli/index.js
@@ -1,31 +1,60 @@
-import React from 'react';
-import { Button } from '../../atoms';
+import React, {useState} from 'react';
+// import { Button } from '../../atoms';
+import '../styles.css';
 
 export default function AntrianPoli() {
   
-  
+  const [count, setCount] = useState(0);
   return(
     <main>
       <div className='content'>
-      
-       <Button props={{
-          label:'Cetak Antrian Poli Umum',
-          type:'primary',
-        }} />
-      
-      <br />
-      <Button props={{
-          label:'Cetak Antrian Poli Anak',
-          type:'primary',
-        }} />
+        <center>
+
+        <div className='row my-5'>
+
+          <div className='card text-center my-5'>
+            
+            <div className='content'>
+              <h1> Antrian Poli Umum </h1>
+              <div className='my-5'>
+                <h1 className='my-5'>{count}</h1>
+
+                {/* <Button props={{
+                  label:'Ambil Antrian',
+                  onClick: setCount(count + 1)
+                }} /> */}
+               
+                <button className='btn btn-success mx-3' onClick={() => setCount(count + 1)}> Ambil Antrian </button>
+                <button className='btn btn-secondary mx-3' onClick={() => setCount(0)}>Reset</button>
+              </div>
+            </div>
+
+            <div className='content'>
+              <h1> Antrian Poli KIA </h1>
+              <div className='my-5'>
+                <h1 className='my-5'>{count}</h1>
+                <button className='btn btn-success mx-' onClick={() => setCount(count + 1)}> Ambil Antrian </button>
+                <button className='btn btn-secondary mx-3' onClick={() => setCount(0)}>Reset</button>
+              </div>
+            </div>
+
+            <div className='content'>
+              <h1> Antrian Poli Gigi </h1>
+              <div className='my-5'>
+                <h1 className='my-5'>{count}</h1>
+                <button className='btn btn-success mx-3' onClick={() => setCount(count + 1)}> Ambil Antrian </button>
+                <button className='btn btn-secondary mx-3' onClick={() => setCount(0)}>Reset</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        </center>
         
-      <br />
-      <Button props={{
-          label:'Cetak Antrian Poli Gigi',
-          type:'primary',
-        }} />
-      
       </div>
+        
+
     </main>
+    
   );
 }
